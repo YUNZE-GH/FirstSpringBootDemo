70d0cf9485de8ac8a3ec63f811b733a5acc1bc01
gaoh <834363368@qq.com>
2020-07-31 15:55:18 +0800

    异常信息输出

--- src/main/java/com/gh/firstdemo/controller/GlobalExceptionHandlerController.java
+++ src/main/java/com/gh/firstdemo/controller/GlobalExceptionHandlerController.java
@@ -1,6 +1,8 @@
 package com.gh.firstdemo.controller;
 
 import com.gh.firstdemo.utils.MyException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,9 +16,11 @@ import java.sql.SQLException;
  */
 @ControllerAdvice   // 使用@ControllerAdvice注解可以进行全局异常处理、全局数据绑定、全局数据预处理
 public class GlobalExceptionHandlerController {
+    private Log log = LogFactory.getLog(this.getClass().getName());
 
     @ExceptionHandler(value = Exception.class)  // 该注解可以捕获到当前Controller所有的异常
     public String handlerException(Exception e) {
+        log.error(e);
         if (e instanceof SQLException) {
             return "error/sqlerror";
         } else if (e instanceof MyException) {
