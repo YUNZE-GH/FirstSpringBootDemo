71696319591020a0bda460551a075184224878ab
gaoh <834363368@qq.com>
2020-08-02 12:12:31 +0800

    SpringBoot的单元测试

--- src/test/java/com/gh/firstdemo/FirstdemoApplicationTests.java
+++ src/test/java/com/gh/firstdemo/FirstdemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.gh.firstdemo;
 
+import com.gh.firstdemo.entity.BoTaskPlan;
+import com.gh.firstdemo.service.BoTaskPlanService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class FirstdemoApplicationTests {
 
+    @Autowired
+    private BoTaskPlanService service;
+
     @Test
     void contextLoads() {
+        BoTaskPlan bo = service.getOne("22311");
+        System.err.println(bo.toString());
     }
 
 }
